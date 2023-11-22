d475e19e0832c4b8efac124805705ebde98441d1
Brian Andres Suhit <briansuhit@outlook.com>
2023-11-22 18:34:51 -0300

    add logic to move enemy, add dialogue to tutorial level

---
+++ Assets/Scripts/Background/Parallax.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    [SerializeField] private float parallaxSpeed;
+
+    private float spriteHeight;
+    private Vector3 startPos;
+
+    private void Awake()
+    {
+        spriteHeight = GetComponent<SpriteRenderer>().bounds.size.y;
+
+    }
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    void Update()
+    {
+        BackgroundMovement();
+        ResetBackgroundPosition();
+    }
+
+    private void BackgroundMovement()
+    {
+        transform.Translate(Vector3.down * parallaxSpeed * Time.deltaTime);
+    }
+
+    private void ResetBackgroundPosition()
+    {
+        if (transform.position.y < startPos.y - spriteHeight)
+        {
+            transform.position = startPos;
+        }
+    }
+}
