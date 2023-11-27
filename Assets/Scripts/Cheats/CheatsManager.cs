35977196468fa23a1a4bc57c0d29b0d81da4051c
Brian Andres Suhit <briansuhit@outlook.com>
2023-11-27 17:43:41 -0300

    add DamageAOE cheat

--- Assets/Scripts/Cheats/CheatsManager.cs
+++ Assets/Scripts/Cheats/CheatsManager.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,12 +8,22 @@ public class CheatsManager : MonoBehaviour
     private Action InvinsibleEvent;
 
     [SerializeField] private HealthPoints healthPoints;
+    [SerializeField] private List<HealthPoints> enemiesListCount = new List<HealthPoints>();
+    [SerializeField] private int damageAOE = 5;
+
 
     public void Awake()
     {
         if (!healthPoints)
         {
             Debug.LogError("player has no heal");
+        }
+
+        if (enemiesListCount.Count == 0)
+        {
+            Debug.LogError($"{name}: enemies list is empty\n Check this assignment list\n disable component");
+            enabled = false;
+            return;
         }
     }
 
@@ -28,6 +39,37 @@ public class CheatsManager : MonoBehaviour
         if (inputContext.started)
         {
             InvinsibleEvent?.Invoke();
+        }
+    }
+
+    public void SetDamageAOE(InputAction.CallbackContext inputContext)
+    {
+        Debug.Log("me estoy apretando");
+
+        if (inputContext.started)
+        {
+            MakeDamage();
+        }
+    }
+
+    public void SetMovementSpeed(InputAction.CallbackContext inputContext)
+    {
+        if (inputContext.started)
+        {
+            SpeedMultiplier();
+        }
+    }
+
+    public void SpeedMultiplier()
+    {
+        Debug.LogError("Need a reference");
+    }
+
+    public void MakeDamage()
+    {
+        for (int i = 0; i < enemiesListCount.Count; i++)
+        {
+            enemiesListCount[i].TakeDamage(damageAOE);
         }
     }
 }
